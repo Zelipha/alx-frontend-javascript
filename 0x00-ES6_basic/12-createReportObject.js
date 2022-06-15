f7836dbe95e21c18e3046eddd95dccbc1f735a7f
Zelipha <zeliphawambui756@gmail.com>
2022-06-15 20:27:28 +0300

    Function that returns value of the previous function

---
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -0,0 +1,8 @@
+export default function createReportObject(employeesList) {
+  return {
+    allEmployees: {
+      ...employeesList,
+    },
+    getNumberOfDepartments: (employeesList) => Object.keys(employeesList).length,
+  };
+}
