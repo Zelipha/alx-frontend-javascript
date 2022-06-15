0fcbe67211f7e021794bf374a7df46117aa15e1e
Zelipha <zeliphawambui756@gmail.com>
2022-06-15 17:49:12 +0300

    Modify variables so as not to be overwritten

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,9 +3,7 @@ export default function taskBlock(trueOrFalse) {
   const task2 = true;
 
   if (trueOrFalse) {
-    const task = true;
-    const task2 = false;
+    return [task, task2];
   }
-
   return [task, task2];
 }
