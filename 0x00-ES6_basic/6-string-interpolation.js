3a36f0b43423914cc27d4f602c8e779b11de404c
Zelipha <zeliphawambui756@gmail.com>
2022-06-15 18:48:55 +0300

    Statement to use a template literal

--- 0x00-ES6_basic/6-string-interpolation.js
+++ 0x00-ES6_basic/6-string-interpolation.js
@@ -7,6 +7,6 @@ export default function getSanFranciscoDescription() {
   };
 
   return `As of ${year}, it was the seventh-highest income county in the United States\
-, with a per capita personal income of${budget.income}. As of 2015, San Francisco \
+, with a per capita personal income of ${budget.income}. As of 2015, San Francisco \
 proper had a GDP of ${budget.gdp}, and a GDP per capita of ${budget.capita}.`;
 }
