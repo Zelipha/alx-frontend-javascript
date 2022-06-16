c34b99edc2d8011fd2d5da06e167bdb007408f59
Zelipha <zeliphawambui756@gmail.com>
2022-06-17 01:21:54 +0300

    Object returned should be an instance of Car not EVCar

---
+++ 0x02-ES6_classes/100-evcar.js
@@ -0,0 +1,14 @@
+import Car from './10-car';
+
+class EVCar extends Car {
+  constructor(brand = '', motor = '', color = '', range = '') {
+    super(brand, motor, color);
+    this._range = range;
+  }
+
+  static get [Symbol.species]() {
+    return Car;
+  }
+}
+
+export default EVCar;
