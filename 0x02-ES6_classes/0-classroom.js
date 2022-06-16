5ea4900ee9e13e1914d8e9fca12ec25d98c1073f
Zelipha <zeliphawambui756@gmail.com>
2022-06-16 18:15:10 +0300

    Implement a class ClassRoom

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -1,5 +1,5 @@
 export default class ClassRoom {
-  constructor(_maxStudentsSize) {
-    this._maxStudentsSize = _maxStudentsSize;
+  constructor(maxStudentsSize) {
+    this._maxStudentsSize = maxStudentsSize;
   }
 }
