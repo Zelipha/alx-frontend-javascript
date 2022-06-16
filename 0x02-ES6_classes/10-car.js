4e1711ae12e0c2b87b2edd9718d3b3c52bd3a541
Zelipha <zeliphawambui756@gmail.com>
2022-06-17 01:17:09 +0300

    Method that returns a new object of the class

---
+++ 0x02-ES6_classes/10-car.js
@@ -0,0 +1,16 @@
+export default class Car {
+  constructor(brand, motor, color) {
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+  }
+
+  static get [Symbol.species]() {
+    return this;
+  }
+
+  cloneCar() {
+    const ModelCar = this.constructor[Symbol.species];
+    return new ModelCar();
+  }
+}
