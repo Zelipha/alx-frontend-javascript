5c7676c77733c4405c7799334a079e2d716302d7
Zelipha <zeliphawambui756@gmail.com>
2022-06-19 22:01:31 +0300

    Handle multiple promises when all settled

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,8 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((values) => values)
+    .catch((err) => err);
+}
