7a872492b9a8117a7c1c3f034c93e2187dc0a39d
Zelipha <zeliphawambui756@gmail.com>
2022-06-21 01:55:11 +0300

    Ceates and returns an array

---
+++ 0x01-ES6_promise/9-try.js
@@ -0,0 +1,12 @@
+export default function guardrail(mathFunction) {
+  const queue = [];
+
+  try {
+    queue.push(mathFunction());
+  } catch (err) {
+    queue.push(err.toString());
+  } finally {
+    queue.push('Guardrail was processed');
+  }
+  return queue;
+}
