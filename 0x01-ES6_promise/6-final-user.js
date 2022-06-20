e262a3079e5e77b345ef701c5bbb9cb8b81257a2
Zelipha <zeliphawambui756@gmail.com>
2022-06-21 01:22:18 +0300

    Modify file

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -3,6 +3,11 @@ import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((values) => values)
-    .catch((err) => err);
+    .then((values) => {
+      const arr = [];
+      for (const item of values) {
+        arr.push({ status: item.status, value: item.value || item.reason });
+      }
+      return arr;
+    });
 }
