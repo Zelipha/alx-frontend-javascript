2873011a900e4189b314e3395164e3ea374f0e55
Zelipha <zeliphawambui756@gmail.com>
2022-06-22 01:15:52 +0300

    Returns a map of groceries

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,10 @@
+export default function groceriesList() {
+  const items = new Map();
+  items.set('Apples', 10);
+  items.set('Tomatoes', 10);
+  items.set('Pasta', 1);
+  items.set('Rice', 1);
+  items.set('Banana', 5);
+
+  return items;
+}
