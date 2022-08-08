53b556b93398e7b04d8dceebb3cf44279ac45889
Zelipha <zeliphawambui756@gmail.com>
2022-08-08 21:33:27 +0300

    Writing a class

---
+++ 0x04-TypeScript/task_1/js/main.js
@@ -0,0 +1,7 @@
+"use strict";
+exports.__esModule = true;
+exports.printTeacher = void 0;
+var printTeacher = function (firstName, lastName) {
+  return firstName[0] + ". " + lastName;
+};
+exports.printTeacher = printTeacher;
---
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -0,0 +1,48 @@
+interface Teacher {
+  readonly firstName: string;
+  readonly lastName: string;
+  fullTimeEmployee: boolean;
+  yearsOfExperience?: number;
+  location: string;
+  contract: boolean;
+  [propName: string]: any;
+}
+
+interface Directors extends Teacher {
+  numberOfReports: number;
+}
+
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+const printTeacher: printTeacherFunction = (firstName, lastName) => {
+  return `${firstName[0]}. ${lastName}`;
+};
+
+interface StudentConstructor {
+  new (firstName: string, lastName: string): StudentClassInterface;
+}
+
+interface StudentClassInterface {
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+const StudentClass: StudentConstructor = class StudentClass implements StudentClassInterface {
+  firstName: string;
+  lastName: string;
+
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+  workOnHomework(): string {
+    return "Currently working";
+  }
+  displayName(): string {
+    return this.firstName;
+  }
+};
+
+export { printTeacher, StudentClass };
